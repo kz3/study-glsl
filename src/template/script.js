ce7a4de9cefc3713ba86e808e4431d3e54f48a57
Kazuki Ito <ito@hitomeida.jp>
2019-07-22 11:51:14 +0900

    007 コンテキストの初期化を実施

---
+++ src/template/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // canvasエレメントを取得
+  var c = document.getElementById('canvas');
+  c.width = 500;
+  c.height = 300;
+
+  // webglコンテキストを取得
+  var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+
+  // canvasを黒でクリア(初期化)する
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  console.log('hello');
+});
